e3553b827604abd3b7c76714bf80476b7cd674fd
Vlad <vlad@useresponse.com>
2018-08-20 19:15:09 +0300

    fixed due date

--- src/main/java/useresponse/atlassian/plugins/jira/condition/MoveButtonCondition.java
+++ src/main/java/useresponse/atlassian/plugins/jira/condition/MoveButtonCondition.java
@@ -5,6 +5,7 @@ import com.atlassian.jira.plugin.webfragment.model.JiraHelper;
 import com.atlassian.jira.user.ApplicationUser;
 import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
 import com.atlassian.sal.api.pluginsettings.PluginSettingsFactory;
+import useresponse.atlassian.plugins.jira.service.SettingsService;
 import useresponse.atlassian.plugins.jira.settings.PluginSettings;
 import useresponse.atlassian.plugins.jira.settings.PluginSettingsImpl;
 import javax.inject.Inject;
@@ -22,6 +23,6 @@ public class MoveButtonCondition extends AbstractWebCondition {
     @Override
     public boolean shouldDisplay(ApplicationUser applicationUser, JiraHelper jiraHelper) {
         PluginSettings pluginSettings = new PluginSettingsImpl(pluginSettingsFactory);
-        return !Boolean.parseBoolean(pluginSettings.getAutosendingFlag());
+        return !Boolean.parseBoolean(pluginSettings.getAutosendingFlag()) && SettingsService.testURConnection(pluginSettingsFactory);
     }
 }
