7e878f38e08b531130ec2def8c088e12ebf42387
Vlad <vlad@useresponse.com>
2018-08-22 16:25:06 +0300

    statuses receive

--- src/main/java/useresponse/atlassian/plugins/jira/servlet/IssueServlet.java
+++ src/main/java/useresponse/atlassian/plugins/jira/servlet/IssueServlet.java
@@ -1,11 +1,12 @@
 package useresponse.atlassian.plugins.jira.servlet;
 
-
-
 import com.atlassian.jira.component.ComponentAccessor;
-import com.atlassian.jira.issue.AttachmentManager;
-import com.atlassian.jira.issue.attachment.CreateAttachmentParamsBean;
-import com.atlassian.jira.web.util.AttachmentException;
+import com.atlassian.jira.config.DefaultStatusManager;
+import com.atlassian.jira.event.type.EventDispatchOption;
+import com.atlassian.jira.issue.MutableIssue;
+import com.atlassian.jira.issue.status.Status;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServlet;
@@ -15,17 +16,41 @@ import java.io.IOException;
 
 public class IssueServlet extends HttpServlet {
 
+    Logger log = LoggerFactory.getLogger(IssueServlet.class);
+
     public IssueServlet() {
+
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        AttachmentManager attachmentManager = ComponentAccessor.getAttachmentManager();
-        CreateAttachmentParamsBean ab = new CreateAttachmentParamsBean.Builder().build();
-        try {
-            attachmentManager.createAttachment(ab);
-        } catch (AttachmentException e) {
-            e.printStackTrace();
+
+        String statusName = req.getParameter("status_name");
+        String issueKey = req.getParameter("issue_key");
+
+        log.error(statusName);
+        log.error(issueKey);
+
+        DefaultStatusManager statusManager = ComponentAccessor.getComponent(DefaultStatusManager.class);
+
+
+        for (Status status : statusManager.getStatuses()) {
+            if (status.getName().equals(statusName)) {
+                MutableIssue issue = ComponentAccessor.getIssueManager().getIssueByCurrentKey(issueKey);
+                issue.setStatus(status);
+                ComponentAccessor.getIssueManager().updateIssue(
+                        ComponentAccessor.getJiraAuthenticationContext().getLoggedInUser(),
+                        issue,
+                        EventDispatchOption.DO_NOT_DISPATCH,
+                        false
+                );
+                return;
+            }
         }
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPut(req, resp);
     }
 }
